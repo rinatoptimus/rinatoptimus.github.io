6656c4e6921aa0bce9eba6779294c0850036ad4b
Ilnur Shavaliev <shavalievoficial@gmail.com>
2017-03-27 18:21:53 +0300

    Изменена кнопка Предложить своё фото.

---
+++ abkhazia/app/js/upload-btn.js
@@ -0,0 +1,8 @@
+$(function() {
+    $(function(){
+        $("#upload_link").on('click', function(e){
+            e.preventDefault();
+            $("#upload:hidden").trigger('click');
+        });
+    });
+});
